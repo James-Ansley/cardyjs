bda61eab653552111270d8a10a3e91ca28e8cde2
James Ansley <31982662+James-Ansley@users.noreply.github.com>
2025-06-01 21:32:10 +1200

    one of these days these types will be fixed

--- src/main.d.ts
+++ src/main.d.ts
@@ -13,14 +13,77 @@
  * limitations under the License.
  */
 
-import type {distance} from "./distance.d.ts";
-import type {clique} from "./clique.d.ts";
-import type {neighbourhood} from "./neighbourhood.d.ts";
-import type {orthogonality} from "./orthogonality.d.ts";
+import type {Selector, Strategy} from "./clique.d.ts";
 
-export type {
-    distance,
-    clique,
-    neighbourhood,
-    orthogonality,
-};
+/**
+ * Computes the d-clique centred around the given probe sort using the given
+ * heuristic strategy.
+ *
+ * The card sorts collection does not need to contain the probe sort. The probe
+ * sort will not be included in the result in this case.
+ *
+ * The strategy is a heuristic used to select candidate card sorts to add to
+ * the clique. See <https://doi.org/10.1111/j.1468-0394.2005.00304.x> for more.
+ *
+ * @template K
+ * @template T
+ * @param {number} d The max distance between any two sorts in the clique
+ * @param {Set<T>[]} probe The starting probe sort
+ * @param {Map<K, Set<T>[]>} sorts The collection of card sorts to search for the clique in
+ * @param {Object} options
+ * @param {Strategy<K, T>} options.strategy The heuristic strategy for
+ * selecting candidates to add to the clique.
+ * @param {Selector<K>} options.selector An object to resolve collisions in
+ * selected candidates in the heuristic function
+ * @returns A d-clique around the probe sort
+ */
+export function clique<K, T>(
+    d: number,
+    probe: Set<T>[],
+    sorts: Map<K, Set<T>[]>,
+    options?: { strategy?: Strategy<K, T>; selector: Selector<K> },
+): Set<K>;
+
+/**
+ * Computes the edit distance between the two given card sorts.
+ *
+ * @template T
+ * @param {Set<T>[]} sort1
+ * @param {Set<T>[]} sort2
+ */
+export function distance<T>(
+    sort1: Set<T>[],
+    sort2: Set<T>[],
+): number;
+
+/**
+ * Returns the d-neighbourhood of the given probe sort in the sorts iterable.
+ *
+ * The probe sort does not need to be one of the given sorts and will not be
+ * included in the result if it is not.
+ *
+ * @template K
+ * @template T
+ * @param {number} d The max distance neighbourhood elements and the probe
+ * @param {Set<T>[]} probe The sort at the centre of the neighbourhood
+ * @param {Map<K, Set<T>[]>} sorts A collection of sorts in which to search
+ * for the neighbourhood
+ * @returns {Set<K>} The d-neighbourhood of the given probe
+ */
+export function neighbourhood<K, T>(
+    d: number,
+    probe: Set<T>[],
+    sorts: Map<K, Set<T>[]>,
+): Set<K>;
+
+/**
+ * Returns the orthogonality of the given collection of sorts.
+ *
+ * See: https://doi.org/10.1111/j.1468-0394.2005.00305.x
+ *
+ * @template T
+ * @param {Set<T>[][]} sorts
+ */
+export function orthogonality<T>(
+    sorts: Set<T>[][],
+): number;
